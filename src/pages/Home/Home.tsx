73175f0068d89a6f2626e2fe726db99a2d65f194
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-03-16 13:35:59 -0300

    configured project

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+export function Home() {
+  return (
+    <div className="w-full min-h-screen flex justify-center">
+      <div className="">
+        <h1>Aegende seu corte</h1>
+      </div>
+    </div>
+  );
+}
