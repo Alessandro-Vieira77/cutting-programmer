783c69e5e7ff5d57e16ac6a9c6e257ca10e76893
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-03-17 23:59:25 -0300

    Container time completed

--- src/components/buttonService/ButtonService.tsx
+++ src/components/buttonService/ButtonService.tsx
@@ -8,7 +8,7 @@ export function ButtonService({ court }: PropCourt) {
   return (
     <div className="flex items-center justify-between w-full h-9 px-4 bg-white rounded-xl font-medium">
       {court}
-      <IoAdd size={30} color="#0D2839" />
+      <IoAdd className="cursor-pointer" size={30} color="#0D2839" />
     </div>
   );
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,6 +1,5 @@
 import location from "/img/location.png";
-import { IoAdd } from "react-icons/io5";
-import { ButtonService } from "../../components/ButtonService";
+import { ButtonService } from "../../components/buttonService/ButtonService";
 
 export function Home() {
   return (
@@ -15,7 +14,7 @@ export function Home() {
       <div className="flex justify-center w-full">
         <main className="flex flex-col items-center w-full max-w-5xl border-2 border-green-400 mt-7">
           <div className="w-full max-w-2xl h-48 md:w-full md:max-w-4xl md:h-60 bg-[url(/img/banner.png)]  bg-cover bg-center flex flex-col justify-end   border-2 border-amber-300 ml-4 mr-4">
-            <p className="ml-7 mb-7 w-44 text-white">
+            <p className="ml-2.5 md:ml-7 mb-2 md:mb-7 w-44 text-white text-sm md:text-base">
               [Nome da Barbearia] Santa Luzia - MA
             </p>
           </div>
@@ -55,8 +54,8 @@ export function Home() {
             </div>
             {/* Description, contact and temps */}
             <div className="flex flex-col gap-7 w-full">
-              <div className="hidden md:flex text-xs justify-center items-center text-justify px-2 w-full h-24  bg-azul rounded-lg">
-                {/* description */}
+              {/* description */}
+              <div className="hidden md:flex text-xs justify-center items-center text-justify px-4 py-4 w-full   bg-azul rounded-lg">
                 <p className="lg:text-sm text-xs text-white">
                   Na [Nome da Barbearia], acreditamos que um bom corte de cabelo
                   e barba vão além da estética – é um momento de cuidado, estilo
@@ -65,16 +64,51 @@ export function Home() {
                   proporcionam um atendimento personalizado.
                 </p>
               </div>
-              <div className="w-full bg-azul px-2 py-2 rounded-lg text-base md:text-2xl">
+              {/* location */}
+              <div className="w-full bg-azul px-4 py-4 rounded-lg text-base md:text-2xl">
                 <h2 className="text-white font-medium  mb-4">Contato</h2>
-                <img
-                  className="w-full bg-cover bg-center mb-4"
-                  src={location}
-                  alt="localização"
-                />
+                <a
+                  className="w-full"
+                  href="https://maps.app.goo.gl/zTED8whM5cFkbYZk6"
+                  target="_blank"
+                >
+                  <img
+                    className="w-full bg-cover bg-center mb-4"
+                    src={location}
+                    alt="localização"
+                  />
+                </a>
                 <p className="text-white font-medium">(98) 985467978</p>
               </div>
-              <div className="w-full h-35.75  border-2 border-blue-400"></div>
+              {/* Horário */}
+              <div className=" flex flex-col items-center gap-4 w-full  px-4 py-4 bg-azul text-white rounded-2xl">
+                <h2 className="text-base md:text-2xl">
+                  Horário de funcionamento
+                </h2>
+                <div className="w-20 md:w-24 bg-green-500 rounded-md px-1 py-1 text-center text-sm md:text-base">
+                  Aberto
+                </div>
+                <div className="flex w-full justify-between">
+                  <div className="flex flex-col gap-3.5 font-medium text-sm md:text-base">
+                    <p>segunda-feira</p>
+                    <p>terça-feira</p>
+                    <p>quarta-feira</p>
+                    <p>quinta-feira</p>
+                    <p>sexta-feira</p>
+                    <p>sábado</p>
+                    <p>domingo</p>
+                  </div>
+                  <div className="flex flex-col gap-3.5 font-medium text-sm md:text-base">
+                    <p>8:00 - 18:30</p>
+                    <p>8:00 - 18:30</p>
+                    <p>8:00 - 18:30</p>
+                    <p>8:00 - 18:30</p>
+                    <p>8:00 - 18:30</p>
+                    <p>8:00 - 17:30</p>
+                    <p>8:00 - 12:00</p>
+                  </div>
+                </div>
+              </div>
             </div>
           </section>
         </main>
