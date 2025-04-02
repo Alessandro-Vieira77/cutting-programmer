d04ef53b18e99e50af4617fd1a24182332cd8a13
Alessandro-Vieira77 <alessandrovieiraa77@gmail.com>
2025-04-02 11:17:06 -0300

    protected api key

--- src/service/db.ts
+++ src/service/db.ts
@@ -1,12 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBIe-47sjipb14ClnDvCy1d2tnLqUwY42Y",
+  apiKey: import.meta.env.API_KEY,
   authDomain: "barbeiro-bc4b9.firebaseapp.com",
   projectId: "barbeiro-bc4b9",
   storageBucket: "barbeiro-bc4b9.firebasestorage.app",
@@ -14,7 +9,6 @@ const firebaseConfig = {
   appId: "1:884415725298:web:6f91f2154f6fc9f79e1a6b",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
